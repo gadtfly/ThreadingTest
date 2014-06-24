4b782d9501161a02744b033207d28687fddb9a1a
Adam Louis <adamlouis@gmail.com>
2014-06-23 21:04:09 -0700

    using closure_tree... hash_tree loses natural structure, which sucks

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def index
-    @comments = Comment.where(parent: nil)
+    @comments = Comment.hash_tree
   end
 
   def create
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140624005902) do
+ActiveRecord::Schema.define(version: 20140624020849) do
+
+  create_table "comment_hierarchies", id: false, force: true do |t|
+    t.integer "ancestor_id",   null: false
+    t.integer "descendant_id", null: false
+    t.integer "generations",   null: false
+  end
+
+  add_index "comment_hierarchies", ["ancestor_id", "descendant_id", "generations"], name: "comment_anc_desc_udx", unique: true
+  add_index "comment_hierarchies", ["descendant_id"], name: "comment_desc_idx"
 
   create_table "comments", force: true do |t|
     t.text     "body"
